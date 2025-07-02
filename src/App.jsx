2624ca72fbbb36aaf6ad61f7af764c1008f71fe5
Bhavik 9696 <raibhavik7619325139@gmail.com>
2025-07-02 14:53:44 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect,useRef } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,9 +7,17 @@ function App() {
   const [length, setlength] = useState(8);
   const [numberAllowed, setnumberAllowed] = useState(false);
   const [charAllowed, setcharAllowed] = useState(false);
-  const [Passward, setpassward] = useState("");
+  const [password, setpassword] = useState("");
 
-  const passwardGenerator = useCallback(() => {
+
+  const passwordRef=useRef(null)
+
+  const copypasswordtoClipboard=useCallback(()=>{
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password);
+  },[password])
+
+  const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
@@ -18,37 +26,56 @@ function App() {
 
     for (let i = 1; i < length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
 
-    setpassward(pass);
-  }, [length, numberAllowed, charAllowed, setpassward]);
+    setpassword(pass);
+  }, [length, numberAllowed, charAllowed, setpassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, setpassword]);
 
   return (
     <div className="container">
-      <h1>Passward Generator</h1>
+      <h1>password Generator</h1>
       <div className="passgen">
-        <input type="text"
-        readOnly 
-        value={Passward}
-        placeholder="passward"
+        <input
+          type="text"
+          readOnly
+          value={password}
+          placeholder="password"
+          ref={passwordRef}
         ></input>
-        <button>copy</button>
+        <button onClick={copypasswordtoClipboard}>copy</button>
       </div>
       <div className="dependence">
-
-
-        <input type="range" min={6} max={99} value={length} onChange={(e)=>{setlength(e.target.value)}}/>
-
+        <input
+          type="range"
+          min={6}
+          max={99}
+          value={length}
+          onChange={(e) => {
+            setlength(e.target.value);
+          }}
+        />
 
         <label>length:{length}</label>
-        <input type="checkbox" defaultChecked={numberAllowed} onChange={()=>{
-          setnumberAllowed((prev)=>!prev);
-        }}/>
+        <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          onChange={() => {
+            setnumberAllowed((prev) => !prev);
+          }}
+        />
         <label>number wanted?</label>
-        <input type="checkbox" defaultChecked={charAllowed} onChange={()=>{
-          setnumberAllowed((prev)=>!prev);
-        }}/>
+        <input
+          type="checkbox"
+          defaultChecked={charAllowed}
+          onChange={() => {
+            setcharAllowed((prev) => !prev);
+          }}
+        />
         <label>characters wanted?</label>
       </div>
     </div>
